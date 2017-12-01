a8fd9c1e09e8be6dfdef0c8e605df89168f2916c
zinuzian <zinuzian@naver.com>
2017-12-01 18:34:59 +0900

    Update ImageAlgo.java

--- app/src/main/java/com/example/user/emocation/ImageAlgorithm/ImageAlgo.java
+++ app/src/main/java/com/example/user/emocation/ImageAlgorithm/ImageAlgo.java
@@ -34,54 +34,75 @@ public class ImageAlgo {
     public ImageAlgo(Bitmap bitmap, Emotion emotion) {
         this.bitmap = bitmap;
         this.emotion = emotion;
+        
         ImageStat status = analysis();
         backgroundValue = new BGvalue();
+        totalValue = new Emotion(emotion);
+        
         AdjByMainColor(status.getMainColors());
         AdjByContrast(status.getContrast(status.getHisto(0)));
-        AdjBySaturation(status.getSaturation());
         AdjByTemperature(status.getTemp());
+        AdjBySaturation(status.getSaturation());
     }
 
     
     
 
-    public ImageStat analysis(){        //analize the img to find some information about img
-        ImageStat imgstat = null;
-        try {
-            imgstat = new ImageStat(bitmap);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return imgstat;
-    }
+//    public ImageStat analysis(){        //analize the img to find some information about img
+//        ImageStat imgstat = null;
+//        try {
+//            imgstat = new ImageStat(bitmap);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return imgstat;
+//    }
 
-    private void AdjByMainColor(String[] mainColors) {  //modify backgroundValue by main 3 colors that is obtained by analizing
+    private void AdjByMainColor(String[] mainColors) {  
+        //modify backgroundValue and total emotion value by main 3 colors that is obtained by analizing
         double vital=0,tempo=0,mordern=0;
+        Emotion temp = new Emotion();
+        
         for (int i = 1; i < 4; i++) {
             if (mainColors[i - 1].compareTo("red") == 0) {  
                 vital += (0.04 / i);
                 tempo += (0.02 / i);
                 mordern -= (0.02 / i);
+                temp.anger += (0.04 / i);
+                temp.fear += (0.02 / i);
+                temp.sadness -= (0.02 / i);
             } else if (mainColors[i - 1].compareTo("yellow") == 0) {
                 vital += (0.04 / i);
                 tempo += (0.04 / i);
                 mordern -= (0.03 / i);
+                temp.happiness += (0.02 / i);
+                temp.surprise += (0.04 / i);
             } else if (mainColors[i - 1].compareTo("green") == 0) {
                 vital += (0.02 / i);
                 tempo -= (0.03 / i);
                 mordern -= (0.04 / i);
+                temp.anger -= (0.03 / i);
+                temp.neutral += (0.05 / i);
+                temp.happiness += (0.02 / i);
             } else if (mainColors[i - 1].compareTo("blue") == 0) {
                 vital -= (0.03 / i);
                 tempo -= (0.05 / i);
                 mordern -= (0.03 / i);
+                temp.neutral += (0.05 / i);
+                temp.sadness += (0.02 / i);
             } else if (mainColors[i - 1].compareTo("white") == 0) {
                 vital += (0.01 / i);
                 tempo -= (0.04 / i);
                 mordern += (0.03 / i);
+                temp.neutral += (0.03 / i);
+                temp.fear += (0.01 / i);
             } else if (mainColors[i - 1].compareTo("black") == 0) {
                 vital -= (0.03 / i);
                 tempo -= (0.05 / i);
                 mordern += (0.03 / i);
+                temp.fear += (0.05 / i);
+                temp.sadness += (0.01 / i);
+                temp.neutral += (0.01 / i);
             }
 //			else if(mainColors[i-1].compareTo("gray") == 0){
 //				
@@ -90,35 +111,57 @@ public class ImageAlgo {
                 vital += (0.01 / i);
                 tempo -= (0.02 / i);
                 mordern += (0.02 / i);
+                temp.sadness += (0.05 / i);
+                temp.surprise += (0.02 / i);
+                temp.fear += (0.01 / i);
             } else if (mainColors[i - 1].compareTo("brown") == 0) {
                 vital -= (0.05 / i);
                 tempo += (0.03 / i);
                 mordern -= (0.03 / i);
+                temp.neutral += (0.06 / i);
+                temp.happiness += (0.02 / i);
             }
         }
+        
         backgroundValue.add(new BGvalue(vital,tempo,mordern));
+        totalValue.addEmotionValue(temp);
     }
 
     private void AdjByContrast(double contrast) {   //modify backgroundValue by contrast that is obtained by analizing
         double vital=0,tempo=0,mordern=0;
+        Emotion temp = new Emotion();
+        
         vital -= (1-contrast)/10;
         tempo += (contrast-0.5)/10;
         mordern += (1-contrast)/10;
+        temp.happiness += (0.03 * contrast);
+        temp.surprise += (0.01 * contrast);
 
         backgroundValue.add(new BGvalue(vital,tempo,mordern));
+        totalValue.addEmotionValue(temp);
     }
 
     private void AdjBySaturation(double saturation) {   //modify backgroundValue by saturation that is obtained by analizing
         double vital=0,tempo=0,mordern=0;
+        
         vital -= (1-saturation)/10;
         tempo -= saturation/100;
         mordern += saturation/10;
+        
         backgroundValue.add(new BGvalue(vital,tempo,mordern));
+        totalValue.mulEmotionValue(saturation);
     }
 
     private void AdjByTemperature(double t) {   //modify backgroundValue by temperature that is obtained by analizing
         double vital=0,tempo=t/10,mordern=0;
+        Emotion temp = new Emotion();
+        
+        temp.anger -= (t / 10);
+        temp.sadness -= (t / 10);
+        temp.fear -= (t / 10);
+        
         backgroundValue.add(new BGvalue(vital,tempo,mordern));
+        totalValue.addEmotionValue(temp);
     }
 
     public Emotion getEmotion() {
